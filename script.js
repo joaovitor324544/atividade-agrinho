13fd99a9fc82e0eec5e159735fc669eb8697cde2
joaovitor324544 <140770599+joaovitor324544@users.noreply.github.com>
2025-05-19 11:04:59 +0000

    ave.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+        opcoesAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
+    
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+    
+    let tamanhoAtualFonte = 1;
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    diminuirFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+})
